6f7385a3afe3fb6f37027bf39e80d19f2fa1eb26
PabloMateosP <pmatpal0105@g.educaand.es>
2023-10-22 14:12:34 +0200

    PabloMP || Subida archivos tarea 1.14 y .JSON

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,7 +15,7 @@ export class HomePage {
   constructor(private httpClient:HttpClient) {
 
     // httpClient es un objeto de una librería que se encarga de hacer operaciones con http
-    this.users = this.httpClient.get('https://');
+    this.users = this.httpClient.get('https://raw.githubusercontent.com/PabloMateosP/ACTIVIDAD1.14/master/src/app/home/MOCK_DATA.json');
 
   }
 
