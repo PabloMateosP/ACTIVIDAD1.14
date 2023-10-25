bb8cd9a1cf82b8e4ab71e2213a3c799c7eb442d8
unknown <pablomp1301@gmail.com>
2023-10-25 10:35:46 +0200

    PabloMP || Subida tarea 1.14

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { map } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   
   users: any;
@@ -17,6 +16,10 @@ export class HomePage {
     // httpClient es un objeto de una librería que se encarga de hacer operaciones con http
     this.users = this.httpClient.get('https://raw.githubusercontent.com/PabloMateosP/ACTIVIDAD1.14/master/src/app/home/MOCK_DATA.json');
 
+  }
+
+  borrarContacto(){
+    this.users = [];
   }
 
 }
